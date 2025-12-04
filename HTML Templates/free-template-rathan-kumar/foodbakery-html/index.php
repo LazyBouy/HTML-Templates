35aa8ddad6152c2f17bf195047dad70faa5015e8
LazyBouy <i.gourabdeb@gmail.com>
2025-12-04 10:34:47 +0100

    first commit

---
+++ HTML Templates/free-template-rathan-kumar/foodbakery-html/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rathan Kumar - Download Premium Template for Free</title>
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-theme.css" rel="stylesheet">
+    <link href="assets/css/iconmoon.css" rel="stylesheet">
+    <link href="style-demo.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div id="main">
+        <div class="main-section">
+          <div class="page-section" id="myButton" style="padding: 60px 0 10px;">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                  <div class="main-heading algin-center ">
+                    <h2>Pre-built Demos</h2>
+                    <p>5 different homepages for your business. Click on any of the demos to see the complete layouts.</p>
+                  </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                  <ul class="demo-list">
+                    <li class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                      <div class="img-holder">
+                        <figure><a target="_blank" href="index.html"><img src="assets/extra-images/demo-1.jpg" alt=""></a>
+                        </figure>
+                      </div>
+                      <div class="text-holder">
+                        <h3><a target="_blank" href="#">Food Bakery (Multiple Restaurants)</a></h3>
+                      </div>
+                    </li>
+                    <li class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                      <div class="img-holder">
+                        <figure><a target="_blank" href="mexican-restaurant.html"><img src="assets/extra-images/demo-2.jpg" alt=""></a></figure>
+                      </div>
+                      <div class="text-holder">
+                        <h3><a target="_blank" href="#">Mexican Restaurant</a></h3>
+                      </div>
+                    </li>
+                    <li class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                      <div class="img-holder">
+                        <figure><a target="_blank" href="foodstop.html"><img src="assets/extra-images/food-stop.png" alt=""></a></figure>
+                      </div>
+                      <div class="text-holder">
+                        <h3><a target="_blank" href="#">Food Stop</a></h3>
+                      </div>
+                    </li>
+                    <li class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                      <div class="img-holder">
+                        <figure><a target="_blank" href="food-cout.html"><img src="assets/extra-images/foodcourt.jpg" alt=""></a></figure>
+                      </div>
+                      <div class="text-holder">
+                        <h3><a target="_blank" href="#">Food Court</a></h3>
+                      </div>
+                    </li>
+                    <li class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                      <div class="img-holder">
+                        <figure><a target="_blank" href="rtl.html"><img src="assets/extra-images/Foodbakery-RTL.jpg" alt=""></a></figure>
+                      </div>
+                      <div class="text-holder">
+                        <h3><a target="_blank" href="#">RTL</a></h3>
+                      </div>
+                    </li>
+                    
+                  </ul>
+                </div>
+
+                
+              </div>
+              <div class="text-center">
+                <a href="#" class="btn btn-danger btn-lg" style="width:300px;font-size: 25px !important;"><i class="fa fa-download" aria-hidden="true"></i>
+ Download all for FREE</a>
+              </div>
+            </div>
+          </div>
+          <br>
+          <br>
+          <br>
+          
+        </div>
+        <!-- Main End -->
+      </div>
+</body>
+</html>
